782f63bfec803728da6384d03799f21db514c22c
AvionShea <avionsheadesigns@gmail.com>
2024-02-03 15:10:40 -0500

    added reset choice

--- js/Wallmain.js
+++ js/Wallmain.js
@@ -3,6 +3,7 @@ document.getElementById('solstice').onclick = partySolstice
 document.getElementById('spellbound').onclick = partySpellbound
 document.getElementById('pineGrove').onclick = partyPineGrove
 document.getElementById('jacaranda').onclick = partyJacaranda
+document.getElementById('reset').onclick = resetWallpaper
 
 function partyHeliotrope() {
     document.querySelector('img').style.backgroundColor = 'rgba(165, 124, 142, 1)'
@@ -18,4 +19,7 @@ function partyPineGrove() {
 }
 function partyJacaranda() {
     document.querySelector('img').style.backgroundColor = 'rgba(120, 101, 123, 1)'
+}
+function resetWallpaper() {
+    document.querySelector('img').style.backgroundColor = 'rgba(255, 255, 255, 1)'
 }
